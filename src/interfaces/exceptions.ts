4802cf34814410734718eef3e91d32ab6cde6682
Zerapium <kimtaehyung2451982@gmail.com>
2021-11-18 15:21:03 +0530

    Add Login & Unathorized exceptions

---
+++ src/interfaces/exceptions.ts
@@ -0,0 +1,34 @@
+class UnauthorizedException extends Error {
+    constructor(msg?: string) {
+      super();
+  
+      this.name = "UnauthorizedException";
+      this.message = msg ? msg : "You are not authorized";
+      // Set the prototype explicitly.
+      Object.setPrototypeOf(this, UnauthorizedException.prototype);
+    }
+  }
+  
+  class LoginException extends Error {
+    constructor(msg?: string) {
+      super();
+  
+      this.name = "LoginException";
+      this.message = msg ? msg : "Something went wrong while logging in";
+      // Set the prototype explicitly.
+      Object.setPrototypeOf(this, LoginException.prototype);
+    }
+  }
+  
+  class NotFoundException extends Error {
+    constructor(msg?: string) {
+      super();
+      this.name = "NotFoundException";
+      this.status = "404";
+      this.message = msg ? msg : "Not Found";
+      // Set the prototype explicitly.
+      Object.setPrototypeOf(this, NotFoundException.prototype);
+    }
+  }
+  
+  export { UnauthorizedException, LoginException, NotFoundException };
