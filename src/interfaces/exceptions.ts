8ced5d4ea504bf738b550d0461bf4581819db322
Zerapium <kimtaehyung2451982@gmail.com>
2021-11-20 10:34:51 +0530

    Remove status code from NotFoundException

--- src/interfaces/exceptions.ts
+++ src/interfaces/exceptions.ts
@@ -1,34 +1,33 @@
 class UnauthorizedException extends Error {
-    constructor(msg?: string) {
-      super();
-  
-      this.name = "UnauthorizedException";
-      this.message = msg ? msg : "You are not authorized";
-      // Set the prototype explicitly.
-      Object.setPrototypeOf(this, UnauthorizedException.prototype);
-    }
+  constructor(msg?: string) {
+    super();
+
+    this.name = "UnauthorizedException";
+    this.message = msg ? msg : "You are not authorized";
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, UnauthorizedException.prototype);
   }
-  
-  class LoginException extends Error {
-    constructor(msg?: string) {
-      super();
-  
-      this.name = "LoginException";
-      this.message = msg ? msg : "Something went wrong while logging in";
-      // Set the prototype explicitly.
-      Object.setPrototypeOf(this, LoginException.prototype);
-    }
+}
+
+class LoginException extends Error {
+  constructor(msg?: string) {
+    super();
+
+    this.name = "LoginException";
+    this.message = msg ? msg : "Something went wrong while logging in";
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, LoginException.prototype);
   }
-  
-  class NotFoundException extends Error {
-    constructor(msg?: string) {
-      super();
-      this.name = "NotFoundException";
-      this.status = "404";
-      this.message = msg ? msg : "Not Found";
-      // Set the prototype explicitly.
-      Object.setPrototypeOf(this, NotFoundException.prototype);
-    }
+}
+
+class NotFoundException extends Error {
+  constructor(msg?: string) {
+    super();
+    this.name = "NotFoundException";
+    this.message = msg ? msg : "Not Found";
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, NotFoundException.prototype);
   }
-  
-  export { UnauthorizedException, LoginException, NotFoundException };
+}
+
+export { UnauthorizedException, LoginException, NotFoundException };
