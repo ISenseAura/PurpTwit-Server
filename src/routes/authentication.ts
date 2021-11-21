e7126ad227ece52dfc6ba698c9b106cdd4bf9641
Zerapium <kimtaehyung2451982@gmail.com>
2021-11-21 15:31:16 +0530

    implement user registration and password hashing logic

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import hash = require("bcryptjs");
+import Db from "../db";
 import { UserType, PostType } from "../interfaces/types";
+import { UserExistsException } from "../interfaces/exceptions";
 import { body, validationResult } from "express-validator";
 
 const router: any = express.Router();
@@ -15,11 +18,32 @@ router.post(
       min: 8
     })
   ],
-  (req: any, res: any) => {
+  async (req: any, res: any) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    try {
+      const salt = await hash.genSalt(10);
+      const passwordHash = await hash.hash(req.body.password.trim(), salt);
+      let user = req.body;
+
+      if(Db.emailAlreadyExists(user.email)) {
+        return res.send(new UserExistsException(" That email is already registered"))
+      }
+
+      user.password = passwordHash;
+      Db.data.users.push(user)
+      Db.exportDatabase("users");
+      Db.importUsers();
+      res.send(Db.getUser(user.email));
+    } catch (e) {
+      console.log(e);
+    }
+
+
+
     console.log(req.body);
   }
 );
